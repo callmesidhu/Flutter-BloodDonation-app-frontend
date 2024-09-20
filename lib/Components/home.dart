63f8f18bb46e13a75291e2e1fdf65f41908c227a
S Sidharth <sidharthpunalur@gmail.com>
2024-09-20 10:11:45 +0530

    read data added

--- lib/Components/home.dart
+++ lib/Components/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final CollectionReference donor = FirebaseFirestore.instance.collection("donor");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +29,68 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: StreamBuilder(
+        stream: donor.snapshots(), 
+        builder:(context,AsyncSnapshot snapshot){
+          if(snapshot.hasData){
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length  ,
+              itemBuilder: (context, index){
+                final DocumentSnapshot donorSnap = snapshot.data.docs[index];
+                return Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    height:  80,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 10,
+                          spreadRadius: 15,
+                        )
+                      ]
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            child: CircleAvatar(
+                              backgroundColor: Colors.red,
+                              radius: 30,
+                              child: Text(donorSnap['blood'],
+                              style: TextStyle(fontSize:  25),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                          Text(donorSnap['name'] ,style: TextStyle(
+                            fontSize:  22,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                          Text(donorSnap['phone'].toString(), style: TextStyle(fontSize:  18),)
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
+                            IconButton(onPressed: (){}, icon: Icon(Icons.delete )),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              });
+          }
+            return Container();
+        })
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'Components/home.dart';
 import 'Components/add.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+    if (kIsWeb) {
+    // Provide FirebaseOptions for Web
+    await Firebase.initializeApp(
+            options: FirebaseOptions(
+        apiKey: "AIzaSyAEw6wxfrjSqqa8EHAuLmMtH0h2XpFTW5s",
+        authDomain: "flutter-blooddonation-app.firebaseapp.com",
+        projectId: "flutter-blooddonation-app",
+        storageBucket: "flutter-blooddonation-app.appspot.com",
+        messagingSenderId: "708035593743",
+        appId: "1:708035593743:web:701e5cb1d59bee2be7057a"      
+      ),
+    );
+    print('Web connected');
+  } else {
+    // Initialize Firebase for mobile platforms (Android, iOS)
+    await Firebase.initializeApp();
+  }
   runApp(MyApp());
 }
 
